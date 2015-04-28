8b3ed25285ef2d060bcd091c53d796a1b4b17968
Tommy Phan <tommyphan8@csu.fullerton.edu>
2015-04-28 03:27:14 -0700

    working real-time youtube utube control

--- myapp/app.js
+++ myapp/app.js
@@ -1,70 +1,84 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express"),
+	app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    mongoose = require("mongoose");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+app.use("/public",express.static(__dirname + "/public"));
+//app.use(express.bodyParser());
 
-var app = express();
+var currentSync = {"youtubeID": "", "leader": "", "users" : []};
 
-http = require("http");
-http.createServer(app).listen(3000);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+mongoose.connect('mongodb://localhost/video');
+var db = mongoose.connection;
 
-app.use('/public',  express.static(__dirname + '/public'));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-//routes
-app.get("/", function (req, res) {
-    res.sendFile('public/indexMain.html', {root: __dirname });
+db.on('error', function (err) {
+console.log('connection error', err);
+});
+db.once('open', function () {
+console.log('connected.');
+});
+var videoSchema = mongoose.Schema({
+    name: String,
+    actions: [ String ]
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+var Video = mongoose.model("video", videoSchema);
+
+http.listen(5555, function(){
+  console.log('listening on *:5555');
 });
 
-// error handlers
+io.on('connection', function (socket) {
+    console.log("a user connected");
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    socket.on('play', function (data) {
+        console.log(data);
+        console.log('in server socket event start');
+     
+        console.log("b4 broadcast start emit")
+        socket.broadcast.emit('broadcast play', data);
     });
-  });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    socket.on('stop', function (data) {
+        console.log('in server socket event stop');
+        // save stop action in database
+        console.log("b4 broadcast stop emit")
+        socket.broadcast.emit('broadcast stop', data);
+    });
+
+    socket.on('pause', function (data) {
+        console.log('in server socket event pause');
+        // save pause action in database
+        console.log("b4 broadcast pause emit")
+        socket.broadcast.emit('broadcast pause', data);
+    });
+
+    socket.on('start', function (data) {
+        currentSync = data;
+        console.log(currentSync);
+    });
+  
 });
 
 
-module.exports = app;
+
+
+
+//display action happen for video
+app.get("/videoActions.json", function (req, res) {
+    video.find({}, function (err, result) {
+	   res.json(result);
+    });
+});
+app.get("/", function (req, res) {
+
+    res.sendFile('/public/index.html', {root: __dirname });
+});
+
+app.post("/start", function (req, res) {
+    console.log("start");
+    res.json(currentSync);
+});
+
--- myapp/public/javascripts/yt.js
+++ myapp/public/javascripts/yt.js
@@ -1,21 +1,95 @@
-var main = function () {
-	console.log("loaded");
-	var tag = document.createElement('script');
 
+var main = function () {
+		
+	var currentSync = {"youtubeID": "", "leader": "", "users" : []};
+	
+	//initialize socket.io and connect
+	var socket = io.connect("http://localhost:5555");
+
+	//needed youtube iframe API
+	var tag = document.createElement('script');
 	tag.src = "https://www.youtube.com/iframe_api";
 	var firstScriptTag = document.getElementsByTagName('script')[0];
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 	
+	createCouch(currentSync, socket);
+	initialStart(join);
+
+	socket.on("broadcast play", function(data) {
+		console.log(data);
+		player.seekTo(data);
+		player.playVideo();
+	});
+
+	socket.on("broadcast pause", function(data) {
+		console.log(data);
+		player.pauseVideo();
+		player.seekTo(data);
+		//player.playVideo();
+	});
+
+	$("#play").click(function() {
+		console.log("play");
+		player.playVideo();
+		socket.emit("play", player.getCurrentTime());
+	});
+
+	$("#pause").click(function() {
+		player.pauseVideo();
+		socket.emit("pause", player.getCurrentTime());
+
+	});
+
+	$("leave").click(function() {
+
+	});
+
+
+
+
 
 };
 
-var player;
+var initialStart = function (callback) {
+	$.post("start", {}, function (response) {
+		console.log(response.youtubeID);
+		//currentSync = response;
+		if (response.youtubeID != "") {
+			$("#joinReg").attr('class', 'active');
+			// $("#player").attr('class', 'active');	
+		}
+		else  {
+			$("#registration").attr('class', 'active');
+
+		}
+		callback(response);
+		
+	});
+
+
+};
+
+var join = function (response) {
+	$("#join").click(function() {
+		$("#joinReg").attr('class', 'inactive');
+		$("#player").attr("class", "active");
+		//$("#ytplayer").attr("class", "active");
+		console.log(response.youtubeID);
+		//player.playVideo();
+		player.cueVideoById(response.youtubeID);
+	});
+
+}
+
+
+
+
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 		height: '390',
 		width: '640',
 		playerVars: {
-			controls: 0,
+			controls: 1,
 			disablekb: 1
 		},
 		//videoId: 'M7lc1UVf-VE',
@@ -24,31 +98,70 @@ function onYouTubeIframeAPIReady() {
 			// 'onStateChange': onPlayerStateChange
 		}
 	});
+	//console.log(player);
+
+}
+
+function test() {
+	console.log(player)
+	return player;
 }
 
 function onPlayerReady(event) {
-	player.cueVideoByUrl("https://www.youtube.com/v/DYLbmSp5itA");
-	//event.loadVideoByUrl("https://www.youtube.com/watch?v=nN6gFQMr3yU", 14, "default");
 	
-	//event.target.playVideo();
+}
+
+function cueVideo() {
+
+	return player;
 }
 
 function play() {
-  if (ytplayer) {
-	player.seekTo(20, false)
-	player.playVideo();
-  }
+	if (ytplayer) {
+		socket.emit("play", player.getCurrentTime())
+		player.playVideo();
+	}
 }
 
 function pause() {
-  if (ytplayer) {
-	player.pauseVideo();
-  }
+	if (ytplayer) {
+		player.pauseVideo();
+	}
 }
+
 function stop() {
-  if (ytplayer) {
-	player.stopVideo();
-  }
+	if (ytplayer) {
+		player.stopVideo();
+	}
+}
+
+
+function leave() {
+	$("#name").val("");
+	$("#link").val("");
+	$("#player").attr("class", "inactive");
+	$("#registration").attr("class", "active");
+	player.cueVideoById();
+	
+}
+
+function createCouch(currentSync, socket) {
+	$("#submitLink").click(function() {
+		
+		player.cueVideoById($("#link").val());
+		$("#player").attr("class", "active");
+		$("#registration").attr("class", "inactive");
+		console.log($("#name").text());
+		$(".username").append("<p>").text("Username: " + $("#name").val());
+		$("#username").attr("class", "active");
+		$("#ytplayer").attr("class", "active");
+		currentSync.youtubeID = $("#link").val();
+		currentSync.leader = $("#name").val();
+		currentSync.users.push($("#name").val());
+
+		socket.emit('start', currentSync);
+	});
+	
 }
 
 
